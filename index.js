b3eb3b6f23406a9491fc57f7ef6d0e3f226eda69
August83 <august.a.g@hotmail.se>
2022-12-07 01:07:35 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function validatePassword(){
+    var pswd = document.getElementById("pass1").value;
+ if(pswd =="kakor"){
+window.location.replace ="home.html";
+return true;
+}
+else
+alert("wtf")
+return false;
+}
