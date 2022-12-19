efbe40294b70c6c919b13995c07877949d12fbf8
August83 <august.a.g@hotmail.se>
2022-12-19 17:20:07 +0100

    Add files via upload

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,22 +1,76 @@
 const xClass = "x"
-const circleClass = "circle"
+const oClass = "o"
 const cellElements = document.querySelectorAll("[data-cell]")
-let circleTurn
+const board = document.getElementById("board")
+let oTurn
+const turnText = document.querySelector("#turnText");
+let toggle = true;
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 1, 2],
+    [0, 4, 8],
+    [2, 4, 6]
+]
 
-cellElements.forEach(cell => {
-    cell.addEventListener("click", handleClick,  {once: true} )
+startGame()
+
+function startGame() {
+    oTurn = false
+    cellElements.forEach(cell => {
+        cell.addEventListener("click", handleClick,  {once: true} )
 })
+setBoardHoverClass
+}
 
 function handleClick(event) {
     const cell = event.target
-    const currentClass = circleTurn ? circleClass : xClass
+    const currentClass = oTurn ? oClass : xClass
     placeMark(cell, currentClass)
+    if (checkWin(currentClass)) {
+        endGame(false)
+        return
+    }
     swapTurns()
+    setBoardHoverClass()
+    turnText.textContent = toggle ? "Spelare: o": "Spelare: x"
+    toggle = !toggle;
 }
 
-function placeMark(cell, currentClass){
+function endGame(draw) {
+    if (draw) {
+
+    } else {
+        turnText.textContent = `${oTurn ? "Spelare o vann!" : "Spelare x vann!"}`
+    }
+}
+
+function placeMark(cell, currentClass) {
     cell.classList.add(currentClass)
 }
-function swapTurns(){
-    circleTurn = !circleTurn
+
+function swapTurns() {
+    oTurn = !oTurn;
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(xClass)
+    board.classList.remove(oClass)
+    if (oTurn) {
+        board.classList.add(oClass)
+    } else {
+        board.classList.add(xClass)
+    }
+}
+
+function checkWin(currentClass) {
+    return winningCombinations.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentClass)
+        })
+    })
 }
